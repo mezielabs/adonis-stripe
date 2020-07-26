b211b989c121c3b0f734727228bb6e8ed87e48e5
Chimezie Enyinnaya <meziemichael@gmail.com>
2020-07-26 09:54:07 +0100

    feat: add templates

---
+++ adonis-typings/index.ts
@@ -0,0 +1,24 @@
+/*
+ * @mezielabs/adonis-stripe
+ *
+ * (c) Chimezie Enyinnaya <chimezie@adonismastery.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare module '@ioc:Mezielabs/Stripe' {
+	/**
+	 * Shape of adonis-stripe config.
+	 */
+	export interface StripeConfig {
+		secretKey: string
+		publicKey: string
+		webhookSecret: string
+
+		options: {
+			apiVersion: string
+			typescript?: boolean
+		}
+	}
+}
