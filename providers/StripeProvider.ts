f76f01fcff94de6362921df827fd036a7b688a2f
Chimezie Enyinnaya <meziemichael@gmail.com>
2023-04-24 18:25:01 +0100

    chore: update author email address

--- providers/StripeProvider.ts
+++ providers/StripeProvider.ts
@@ -1,7 +1,7 @@
 /*
  * @mezielabs/adonis-stripe
  *
- * (c) Chimezie Enyinnaya <chimezie@adonismastery.com>
+ * (c) Chimezie Enyinnaya <chimezie@mezielabs.dev>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
